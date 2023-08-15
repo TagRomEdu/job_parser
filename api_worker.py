43d11b0db7b9cea2748da753c51b882d27cd035f
TagRomEdu <qwertystyles@gmail.com>
2023-08-15 15:42:32 +0500

    added sj key and variable with hh's get

--- api_worker.py
+++ api_worker.py
@@ -1,12 +1,32 @@
 from abc import ABC, abstractmethod
+import os
 
 
 class APIWorker(ABC):
+    API_SJ = os.getenv('API_SuperJob')
+    HH_VACANCIES = 'https://api.hh.ru/vacancies'
 
     @abstractmethod
     def get_object(self):
+        """
+        method gets object works with API
+        """
         pass
 
     @abstractmethod
+    def get_vacations(self):
+        """
+        method gets list of vacations
+        """
+        pass
+
+
+class APIHeadHunter(APIWorker):
+    def __init__(self):
+        pass
+
+    def get_object(self):
+        pass
+
     def get_vacations(self):
         pass
