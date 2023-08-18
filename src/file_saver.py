6044dcd209584c27a2a87a6f96f1e2a368c295c8
TagRomEdu <qwertystyles@gmail.com>
2023-08-19 00:38:43 +0500

    added class JSONSaver

--- src/file_saver.py
+++ src/file_saver.py
@@ -1,7 +1,22 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 
 
 class FileSaver(ABC):
+
+    @abstractmethod
+    def add_vacancy(self, vacancy):
+        pass
+
+    @abstractmethod
+    def get_vacancies_by_salary(self, salary):
+        pass
+
+    @abstractmethod
+    def delete_vacancy(self, vacancy):
+        pass
+
+
+class JSONSaver(FileSaver):
 
     def add_vacancy(self, vacancy):
         pass
