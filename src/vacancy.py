9a13fe8ec82437f1696564c0b7be7f5664cc5e3d
TagRomEdu <qwertystyles@gmail.com>
2023-08-19 00:27:41 +0500

    added initialization by post, salary, link, requirements

--- src/vacancy.py
+++ src/vacancy.py
@@ -1,2 +1,7 @@
 class Vacancy:
-    pass
+    def __init__(self, post, salary, link, requirements):
+        self.post = post
+        self.salary = salary
+        self.link = link
+        self.requirements = requirements
+
