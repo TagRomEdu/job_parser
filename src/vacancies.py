58ac9341d4fa8c737051315ee49b941fa8d508cd
TagRomEdu <qwertystyles@gmail.com>
2023-08-22 12:58:39 +0500

    added getters for attrs and comparison methods

--- src/vacancies.py
+++ src/vacancies.py
@@ -1,8 +1,36 @@
 class Vacancy:
-    def __init__(self, name, salary, link, requirements, location):
-        self.name = name
-        self.salary = salary
-        self.url = url
-        self.requirements = requirements
-        self.location = location
+    def __init__(self, name, salary, url, requirements, location, *args):
+        self.__name = name
+        self.__salary = salary
+        self.__url = url
+        self.__requirements = requirements
+        self.__location = location
+        self.list = ','.join(args)
 
+    @property
+    def name(self):
+        return self.__name
+
+    @property
+    def salary(self):
+        return self.__salary
+
+    @property
+    def url(self):
+        return self.__url
+
+    @property
+    def requirements(self):
+        return self.__requirements
+
+    @property
+    def location(self):
+        return self.__location
+
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            return self.salary == other.salary
+
+    def __lt__(self, other):
+        if isinstance(other, self.__class__):
+            return self.salary < other.salary
