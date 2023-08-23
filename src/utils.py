3b74a346401cbf3439fb18efa02f07c9f27ba8be
TagRomEdu <qwertystyles@gmail.com>
2023-08-23 11:57:32 +0500

    added job selection function

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,6 @@
+from src.file_saver import JSONSaver
+
+
 def sort_vac(vacancies_lst, word_lst):
     """
     Sorting by salary, if salary of vac >= salary of func
@@ -14,3 +17,24 @@ def sort_vac(vacancies_lst, word_lst):
                 if word in vacancy["requirements"].replace(',', '').split(" "):
                     sorted_vac_lst.append(vacancy)
     return sorted_vac_lst
+
+
+def job_selection(vacancy_lst):
+
+    json_f_1 = JSONSaver()
+
+    for i in range(len(vacancy_lst)):
+        json_f_1.add_vacancy(vacancy_lst[i])
+
+    salary = int(input("Укажите желаемую стартовую зарплату: "))
+    vac_list = json_f_1.get_vacancies_by_salary(salary)
+
+    filter_words = input("Введите ключевые слова для фильтрации вакансий: ").split(' ')
+    sorted_vacancies = sort_vac(vac_list, filter_words)
+
+    if not sorted_vacancies:
+        print("Таких вакансий нет!")
+
+    for vacancy in sorted_vacancies:
+        print(
+            f'{vacancy["name"]} - {vacancy["salary"]} {vacancy["currency"]} - {vacancy["url"]} - {vacancy["location"]}')
